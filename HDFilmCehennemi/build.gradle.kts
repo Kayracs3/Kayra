51e56929b9a00335b61243bbf498288aa0144249
durukayra96-star <durukayra96@gmail.com>
2026-09-20 14:03:18 +0300

    Update build.gradle.kts

--- HDFilmCehennemi/build.gradle.kts
+++ HDFilmCehennemi/build.gradle.kts
@@ -4,15 +4,7 @@ cloudstream {
     authors     = listOf("hexated", "kayras")
     language    = "tr"
     description = "Türkiye'nin en hızlı hd film izleme sitesi"
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-    **/
-    status  = 1 // will be 3 if unspecified
-    tvTypes = listOf("Movie", "TvSeries")
-    iconUrl = "https://www.google.com/s2/favicons?domain=hdfilmcehennemi.com&sz=%size%"
+    status      = 1
+    tvTypes     = listOf("Movie", "TvSeries")
+    iconUrl     = "https://www.google.com/s2/favicons?domain=hdfilmcehennemi.com&sz=%size%"
 }
