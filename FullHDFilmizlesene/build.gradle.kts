4fe36714d1d60ba39ce18a480d207ccf2500904d
Kayracs3 <durukayra96@gmail.com>
2026-09-20 20:21:06 +0300

    Update build.gradle.kts

--- FullHDFilmizlesene/build.gradle.kts
+++ FullHDFilmizlesene/build.gradle.kts
@@ -1,18 +1,16 @@
+plugins {
+    id("com.android.library")
+    id("kotlin-android")
+    id("cloudstream")
+}
+
 version = 4
 
 cloudstream {
     authors     = listOf("kayracs3")
     language    = "tr"
     description = "Sinema zevkini evinize kadar getirdik. Türkiye'nin lider Film sitesinde, en yeni filmleri Full HD izleyin."
-
-    /**
-     * Status int as the following:
-     * 0: Down
-     * 1: Ok
-     * 2: Slow
-     * 3: Beta only
-    **/
-    status  = 1 // will be 3 if unspecified
+    status  = 1 
     tvTypes = listOf("Movie")
     iconUrl = "https://www.google.com/s2/favicons?domain=www.fullhdfilmizlesene.de&sz=%size%"
 }
