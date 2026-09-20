cc7132214fe6fd5809011ca4f27fc8ae3f1d9580
Kayracs3 <durukayra96@gmail.com>
2026-09-20 19:00:10 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -36,7 +36,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-       setRepo("kayracs3", "Kayra", "builds")
+       setRepo("kayracs3", "Kayra", "github")
 
         authors = listOf("Kayracs3")
     }
