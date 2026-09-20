2d130fc1c5f2c15ed42ac8c3a1fcccb217daccdf
durukayra96-star <durukayra96@gmail.com>
2026-09-20 14:55:27 +0300

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
