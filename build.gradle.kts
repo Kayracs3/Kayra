5ae77062c56da4f51ce6c4ca81fce26cfb26ec3a
Kayracs3 <durukayra96@gmail.com>
2026-09-21 21:23:48 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -46,7 +46,7 @@ subprojects {
 
     cloudstream {
         // when running through github workflow, GITHUB_REPOSITORY should contain current repository name
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/keyiflerolsun/Kekik-cloudstream")
+        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/Kayracs3/Kayra")
 
         authors = listOf("keyiflerolsun")
     }
