d2c851e0909e3bc03a18eaeb8a6f5ba77edfdda6
Kayracs3 <durukayra96@gmail.com>
2026-09-24 21:57:59 +0300

    Change package name in HotlingerExtractor.kt
    Updated package name from 'com.keyiflerolsun' to 'com.Kayracs3'.

--- Dizilla/src/main/kotlin/com/Kayracs3/HotlingerExtractor.kt
+++ Dizilla/src/main/kotlin/com/Kayracs3/HotlingerExtractor.kt
@@ -1,6 +1,6 @@
 // ! Bu araç @keyiflerolsun tarafından | @KekikAkademi için yazılmıştır.
 
-package com.keyiflerolsun
+package com.Kayracs3
 
 class Hotlinger : ContentX() {
     override var name    = "Hotlinger"
