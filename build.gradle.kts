e1c88a3af925f46f9db618527a7bbbc47f2d6849
Kayracs3 <durukayra96@gmail.com>
2026-09-24 21:04:35 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,124 +12,14 @@ buildscript {
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
-    }
-}
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-        maven("https://jitpack.io")
-    }
-}
-
-fun Project.cloudstream(
-    configuration: CloudstreamExtension.() -> Unit
-) {
-    extensions
-        .getByName<CloudstreamExtension>("cloudstream")
-        .configuration()
-}
-
-fun Project.android(
-    configuration: BaseExtension.() -> Unit
-) {
-    extensions
-        .getByName<BaseExtension>("android")
-        .configuration()
-}
-
-subprojects {
-
-    apply(plugin = "com.android.library")
-    apply(plugin = "kotlin-android")
-    apply(plugin = "com.lagradost.cloudstream3.gradle")
-
-    cloudstream {
-        setRepo(
-            System.getenv("GITHUB_REPOSITORY")
-                ?: "Kayracs3/Kayra"
+        // -SNAPSHOT yerine sabit commit
+        classpath(
+            "com.github.recloudstream:gradle:32895aedb6366f5075cb99bbd2e6ce0a7cac325d"
         )
 
-        authors = listOf(
-            "Kayracs3"
+        classpath(
+            "org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0"
         )
     }
-
-    android {
-
-        namespace = "com.Kayracs3"
-
-        defaultConfig {
-            minSdk = 21
-            compileSdkVersion(35)
-            targetSdk = 35
-        }
-
-        compileOptions {
-            sourceCompatibility =
-                JavaVersion.VERSION_11
-
-            targetCompatibility =
-                JavaVersion.VERSION_11
-        }
-
-        tasks.withType<KotlinJvmCompile> {
-
-            compilerOptions {
-
-                jvmTarget.set(
-                    JvmTarget.JVM_11
-                )
-
-                freeCompilerArgs.addAll(
-                    "-Xno-call-assertions",
-                    "-Xno-param-assertions",
-                    "-Xno-receiver-assertions"
-                )
-            }
-        }
-    }
-
-    dependencies {
-
-        val cloudstream by configurations
-
-        val implementation by configurations
-
-        /*
-         * CloudStream pre-release library
-         */
-        cloudstream(
-            "com.lagradost:cloudstream3:pre-release"
-        )
-
-        implementation(
-            kotlin("stdlib")
-        )
-
-        implementation(
-            "com.github.Blatzar:NiceHttp:0.4.11"
-        )
-
-        implementation(
-            "org.jsoup:jsoup:1.18.3"
-        )
-
-        implementation(
-            "com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1"
-        )
-
-        implementation(
-            "com.github.teamnewpipe:NewPipeExtractor:v0.25.2"
-        )
-    }
-}
-
-tasks.register<Delete>("clean") {
-    delete(
-        rootProject.layout.buildDirectory
-    )
 }
