9dbd7602f43c9c0a22b9ae1b0da1af6c994904d4
Kayracs3 <durukayra96@gmail.com>
2026-09-24 20:58:39 +0300

    Update Kotlin and Cloudstream dependencies in build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,19 @@
-import com.lagradost.cloudstream3.gradle.CloudstreamExtension
 import com.android.build.gradle.BaseExtension
+import com.lagradost.cloudstream3.gradle.CloudstreamExtension
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
 
 buildscript {
     repositories {
         google()
         mavenCentral()
-        maven("https://jitpack.io") {
-            metadataSources {
-                mavenPom()
-                artifact()
-            }
-        }
+        maven("https://jitpack.io")
     }
 
     dependencies {
         classpath("com.android.tools.build:gradle:8.7.3")
         classpath("com.github.recloudstream:gradle:-SNAPSHOT")
-        // 1. Kotlin sürümü 2.1.20'ye yükseltildi
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.1.20")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.3.0")
     }
 }
 
@@ -25,32 +21,45 @@ allprojects {
     repositories {
         google()
         mavenCentral()
-        maven("https://jitpack.io") {
-            metadataSources {
-                mavenPom()
-                artifact()
-            }
-        }
+        maven("https://jitpack.io")
     }
 }
 
-fun Project.cloudstream(configuration: CloudstreamExtension.() -> Unit) = 
-    extensions.getByName<CloudstreamExtension>("cloudstream").configuration()
+fun Project.cloudstream(
+    configuration: CloudstreamExtension.() -> Unit
+) {
+    extensions
+        .getByName<CloudstreamExtension>("cloudstream")
+        .configuration()
+}
 
-fun Project.android(configuration: BaseExtension.() -> Unit) = 
-    extensions.getByName<BaseExtension>("android").configuration()
+fun Project.android(
+    configuration: BaseExtension.() -> Unit
+) {
+    extensions
+        .getByName<BaseExtension>("android")
+        .configuration()
+}
 
 subprojects {
+
     apply(plugin = "com.android.library")
     apply(plugin = "kotlin-android")
     apply(plugin = "com.lagradost.cloudstream3.gradle")
 
     cloudstream {
-        setRepo(System.getenv("GITHUB_REPOSITORY") ?: "https://github.com/Kayracs3/Kayra")
-        authors = listOf("Kayracs3")
+        setRepo(
+            System.getenv("GITHUB_REPOSITORY")
+                ?: "Kayracs3/Kayra"
+        )
+
+        authors = listOf(
+            "Kayracs3"
+        )
     }
 
     android {
+
         namespace = "com.Kayracs3"
 
         defaultConfig {
@@ -60,41 +69,67 @@ subprojects {
         }
 
         compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_11
-            targetCompatibility = JavaVersion.VERSION_11
+            sourceCompatibility =
+                JavaVersion.VERSION_11
+
+            targetCompatibility =
+                JavaVersion.VERSION_11
         }
 
-        tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+        tasks.withType<KotlinJvmCompile> {
+
             compilerOptions {
-                jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+
+                jvmTarget.set(
+                    JvmTarget.JVM_11
+                )
+
                 freeCompilerArgs.addAll(
-                    listOf(
-                        "-Xno-call-assertions",
-                        "-Xno-param-assertions",
-                        "-Xno-receiver-assertions",
-                        // 2. Metadata sürüm uyuşmazlığını yok sayan bayrak eklendi
-                        "-Xskip-metadata-version-check"
-                    )
+                    "-Xno-call-assertions",
+                    "-Xno-param-assertions",
+                    "-Xno-receiver-assertions"
                 )
             }
         }
     }
 
     dependencies {
+
         val cloudstream by configurations
+
         val implementation by configurations
 
-        cloudstream("com.lagradost:cloudstream3:pre-release")
+        /*
+         * CloudStream pre-release library
+         */
+        cloudstream(
+            "com.lagradost:cloudstream3:pre-release"
+        )
 
-        implementation(kotlin("stdlib"))
-        implementation("com.github.Blatzar:NiceHttp:0.4.11")
-        implementation("org.jsoup:jsoup:1.18.3")
-        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.16.0")
-        implementation("com.fasterxml.jackson.core:jackson-databind:2.16.0")
-        implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+        implementation(
+            kotlin("stdlib")
+        )
+
+        implementation(
+            "com.github.Blatzar:NiceHttp:0.4.11"
+        )
+
+        implementation(
+            "org.jsoup:jsoup:1.18.3"
+        )
+
+        implementation(
+            "com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1"
+        )
+
+        implementation(
+            "com.github.teamnewpipe:NewPipeExtractor:v0.25.2"
+        )
     }
 }
 
-task<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+tasks.register<Delete>("clean") {
+    delete(
+        rootProject.layout.buildDirectory
+    )
 }
