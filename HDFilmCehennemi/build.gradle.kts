1aa67af5416eb168d4a7804dca35d4e78666b3c2
Kayracs3 <durukayra96@gmail.com>
2026-09-25 22:00:04 +0300

    Add files via upload

---
+++ HDFilmCehennemi/build.gradle.kts
@@ -0,0 +1,16 @@
+version = 1
+
+cloudstream {
+    description = "DiziPal Türkiye katalog ve bölüm sağlayıcısı"
+    authors = listOf("DiziKorea")
+    status = 1
+    tvTypes = listOf("TvSeries")
+    language = "tr"
+    iconUrl = "https://dizikorea3.com//favicon.ico"
+}
+
+android {
+    buildFeatures {
+        buildConfig = true
+    }
+}
