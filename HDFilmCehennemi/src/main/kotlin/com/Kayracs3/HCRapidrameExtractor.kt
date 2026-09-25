7f47bb5530846820233a2cfa167e65002a7daf97
Kayracs3 <durukayra96@gmail.com>
2026-09-26 00:07:24 +0300

    Add HCRapidrameExtractor class for Rapidrame

---
+++ HDFilmCehennemi/src/main/kotlin/com/Kayracs3/HCRapidrameExtractor.kt
@@ -0,0 +1,11 @@
+package com.Kayracs3
+
+class HCRapidrameExtractor :
+    HCCloseLoadExtractor() {
+
+    override var name =
+        "Rapidrame"
+
+    override var mainUrl =
+        "https://www.hdfilmcehennemi.nl"
+}
