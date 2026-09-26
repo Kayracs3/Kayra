d5587a8580f8834db5f6e1e875d1edf0e27673d1
Kayracs3 <durukayra96@gmail.com>
2026-09-26 09:58:33 +0300

    Remove unused imports from InatBoxPlugin.kt

--- InatBox/src/main/kotlin/com/Kayracs3/InatBoxPlugin.kt
+++ InatBox/src/main/kotlin/com/Kayracs3/InatBoxPlugin.kt
@@ -1,8 +1,6 @@
 package com.Kayracs3
 
 import android.content.Context
-import com.keyiflerolsun.CDNJWPlayer
-import com.keyiflerolsun.DiskYandexComTr
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
